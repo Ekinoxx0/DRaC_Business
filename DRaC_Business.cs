109bc6dfc4f53ef7b3b114d7601047cc8e2ffaa9
Eki <fadow0711@gmail.com>
2020-12-08 14:08:07 +0100

    Create DRaC_Business.cs

---
+++ DRaC_Business.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+// Assembly DRaC_Business, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 46ED2638-FF60-4062-85B3-F169AC3C0185
+// Assembly references:
+// mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
+// ScriptHookVDotNet2, Version=2.10.9.0, Culture=neutral, PublicKeyToken=null
+// System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+// System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
+// NativeUI, Version=1.9.0.0, Culture=neutral, PublicKeyToken=null
+// System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
+// Microsoft.CSharp, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyTitle("DRaC_Business")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DRaC_Business")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d0d4bc76-8d5e-403c-a80a-e1f71f2ef6c8")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: TargetFramework(".NETFramework,Version=v4.5.2", FrameworkDisplayName = ".NET Framework 4.5.2")]
+[assembly: AssemblyVersion("1.0.0.0")]
